6611bcb43072c3ee95fc37d7a355bf80fc819aaf
Matheus Henrique <matheustennant@gmail.com>
2021-06-30 16:59:05 -0300

    Arruamando Activity

--- app/src/main/java/com/netlify/joblink/ui/ClientRegisterActivity.kt
+++ app/src/main/java/com/netlify/joblink/ui/ClientRegisterActivity.kt
@@ -1,8 +1,10 @@
 package com.netlify.joblink.ui
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -37,7 +39,7 @@ class ClientRegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_client_register)
+        setContentView(R.layout.activity_register_client)
 
         btnPhotoFreelancer = findViewById(R.id.btn_photo_register)
         imagePhoto = findViewById(R.id.iv_photo_register)
@@ -73,6 +75,13 @@ class ClientRegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun notifyUser(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun selectPhotoGalery() {
+        var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        intent.setType("image/*")
+
+        startActivityForResult(Intent.createChooser(intent, "Escolha uma imagem"), 1)
     }
 
     fun registered() {
